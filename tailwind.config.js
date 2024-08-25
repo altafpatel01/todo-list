72dcb1902b2c6f2ae154f4c38525712e3c76c35a
Altaf <altafpatel8485@gmail.com>
2024-08-25 18:35:20 +0530

    adding tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Update this path according to your setup
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
