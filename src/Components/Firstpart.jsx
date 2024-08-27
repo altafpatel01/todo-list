3248991dcdaa592f26a384552ba990f1a36a1f61
Altaf <altafpatel8485@gmail.com>
2024-08-27 15:26:29 +0530

    update search bar

--- src/Components/Firstpart.jsx
+++ src/Components/Firstpart.jsx
@@ -6,7 +6,7 @@ function Firstpart({handleaddtask , inputvalue , handlesetinputvalue}) {
     <>
     <div className='flex flex-col items-center mt-8 gap-5'>
     <h1 className='text-6xl between:tracking-wider between:text-5xl text-white font-bold font-sans'>Todo List</h1>
-    <input onChange={handlesetinputvalue} placeholder='Enter the task.....' className='placeholder-shown:pl-1 pl-1 between:h-10 between:w-64 w-96 h-10 between:rounded-md rounded-md focus:outline-none'  type="text"  value={inputvalue} />
+    <input onChange={handlesetinputvalue} placeholder='Enter the task.....' className='placeholder-shown:pl-1 pl-1 between:h-10 between:w-62 between:my-2 w-96 h-10 between:rounded-md rounded-md focus:outline-none'  type="text"  value={inputvalue} />
     <button onClick={handleaddtask} className='border px-2 text-gray-500 hover:text-black  py-0.5 rounded-sm bg-white' type="submit">Add task</button>
     </div>
     </>
